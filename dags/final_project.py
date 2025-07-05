bebd6d54416722b5432c4d1bab0b04a2e863f401
anthony_damiano <adamian@wgu.edu>
2025-07-05 13:55:28 -0700

    Updated final project operators

--- dags/final_project.py
+++ dags/final_project.py
@@ -27,34 +27,67 @@ def final_project():
 
     stage_events_to_redshift = StageToRedshiftOperator(
         task_id='Stage_events',
+        table='staging_events',
+        s3_bucket='anthony-damiano',
+        s3_key='log-data',
+        json_path='s3://anthony-damiano/log_json_path.json',
+        redshift_conn_id='redshift',
+        aws_credentials_id='aws_credentials'
     )
 
     stage_songs_to_redshift = StageToRedshiftOperator(
         task_id='Stage_songs',
+        table='staging_songs',
+        s3_bucket='anthony-damiano',
+        s3_key='song-data',
+        json_path='auto',
+        redshift_conn_id='redshift',
+        aws_credentials_id='aws_credentials'
     )
 
     load_songplays_table = LoadFactOperator(
         task_id='Load_songplays_fact_table',
+        redshift_conn_id='redshift',
+        table='songplays',
+        sql_query=SqlQueries.songplay_table_insert
     )
 
     load_user_dimension_table = LoadDimensionOperator(
         task_id='Load_user_dim_table',
+        redshift_conn_id='redshift',
+        table='users',
+        sql_query=SqlQueries.user_table_insert,
+        truncate=True
     )
 
     load_song_dimension_table = LoadDimensionOperator(
         task_id='Load_song_dim_table',
+        redshift_conn_id='redshift',
+        table='songs',
+        sql_query=SqlQueries.song_table_insert,
+        truncate=True
     )
 
     load_artist_dimension_table = LoadDimensionOperator(
         task_id='Load_artist_dim_table',
+        redshift_conn_id='redshift',
+        table='artists',
+        sql_query=SqlQueries.artist_table_insert,
+        truncate=True
     )
 
     load_time_dimension_table = LoadDimensionOperator(
         task_id='Load_time_dim_table',
+        redshift_conn_id='redshift',
+        table='time',
+        sql_query=SqlQueries.time_table_insert,
+        truncate=True
     )
 
     run_quality_checks = DataQualityOperator(
         task_id='Run_data_quality_checks',
+        redshift_conn_id='redshift',
+        tables=['songplays', 'users', 'songs', 'artists', 'time']
     )
 
     end_operator = DummyOperator(task_id='Stop_execution')
