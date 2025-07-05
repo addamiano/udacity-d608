dba01c1beddd96f349fcbe8b1ce5e96b28b8df98
anthony_damiano <adamian@wgu.edu>
2025-07-05 13:29:08 -0700

    Updating args and dependencies

--- dags/final_project.py
+++ dags/final_project.py
@@ -10,6 +10,11 @@ from helpers import SqlQueries
 default_args = {
     'owner': 'udacity',
     'start_date': pendulum.now(),
+    'depends_on_past': False,
+    'retries':3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email_on_retry': False
 }
 
 @dag(
@@ -18,7 +23,6 @@ default_args = {
     schedule_interval='0 * * * *'
 )
 def final_project():
-
     start_operator = DummyOperator(task_id='Begin_execution')
 
     stage_events_to_redshift = StageToRedshiftOperator(
@@ -52,5 +56,16 @@ def final_project():
     run_quality_checks = DataQualityOperator(
         task_id='Run_data_quality_checks',
     )
+
+    end_operator = DummyOperator(task_id='Stop_execution')
+
+    # Task dependencies
+    start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
+    [stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
+    load_songplays_table >> [load_user_dimension_table, load_song_dimension_table, 
+                            load_artist_dimension_table, load_time_dimension_table]
+    [load_user_dimension_table, load_song_dimension_table, 
+     load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks
+    run_quality_checks >> end_operator
 
 final_project_dag = final_project()
